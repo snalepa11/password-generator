7cc3ea77e28a8642a7fc5a5845966e71398e0c63
Snalepa <snalepa1998@gmail.com>
2023-09-21 23:47:48 +0200

    added if else statements for cases and characters and generated final password.

--- script.js
+++ script.js
@@ -10,60 +10,34 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function isEmptyOrSpaces(str){
-  return str === null || str.match(/^ *$/) !== null;
-}
-
-function containsSpecialChars(str) {
-  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  return specialChars.test(str);
-}
-
-
-var extrasObj= {
-
-}
 
 function generatePassword(){
-  console.log("get password")
-  // ask for passwordLength
   var passwordLength = prompt("Enter the desired length of your password")
-
-  if(passwordLength < 8 || passwordLength > 128){
-    passwordLength = prompt("Password must be less greater that 8 or less than 128 characters.")
+  while (Number.parseInt(passwordLength) < 8 || Number.parseInt(passwordLength) > 128){
+    passwordLength = prompt("Password must be greater than 8 or less than 128 characters.")
   }
 
-  var passwordCharacters = prompt("Enter any special characters you wish to use. (Ex: !, @, $)")
+  var confirmCharacters = confirm("Confirm if you would like to use uppercase, lowercaser, numbers, and special characters.")
+  var listOfCharacters = ""
+  
+  if (confirmCharacters) {
+    listOfCharacters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  } else {
+    listOfCharacters= 'abcdefghijklmnopqrstuvwxyz0123456789'
+  }
+  
 
-  if(isEmptyOrSpaces(passwordCharacters)){
-    passwordCharacters=prompt("You must enter at least one special character.")
-  } 
-
-  if(!containsSpecialChars(passwordCharacters)){
-    passwordCharacters=prompt("You must enter at least one special character.")
-  } 
-
-  var extras = confirm("Do you want to use upper case, lower case, numbers, and special characters?")
-  console.log(extras)
- 
   var finalPassword = ""
   for(let i=0; i < passwordLength; i++) {
-
-    
-    finalPassword = finalPassword + "\u0046"
-    // What we want is to append a character in each itteration to the finalpassword string 
+    finalPassword += listOfCharacters.charAt(Math.floor(Math.random() * listOfCharacters.length));
   }
-
-  // var characters = "@#423SDFwer"
-  // finalPassword = finalPassword.concat(characters)
+    
   return finalPassword
 }
 
 
-"!):"
 
 
 
